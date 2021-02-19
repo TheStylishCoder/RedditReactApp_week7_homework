d34c3329fdf5f0bff936a3124cf8daf4b1e1bc22
Anna Cunningham <ac232@hotmail.co.uk>
2021-02-19 15:52:23 +0000

    Added further details

--- src/components/ThreadDetail.js
+++ src/components/ThreadDetail.js
@@ -10,7 +10,12 @@ const ThreadDetail = ({thread, setRedirectToFalse}) => {
     return(
         <>
         <h2>{thread.data.title}</h2>
-        <p>Author:{thread.data.author}</p>
+        <p>Author: {thread.data.author}</p>
+        <p>Type of Hack: {thread.data.link_flair_text}</p>
+        <p>Score on Reddit: {thread.data.score}</p>
+        <p>UpVote Ratio: {thread.data.upvote_ratio}%</p>
+        <p>Number of Awards Received: {thread.data.total_awards_received}</p>
+        <img src={thread.data.thumbnail} height="150" />
         </>
     )
 };
